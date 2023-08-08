0ba056f742b821a3564ec0eeb7b18d02fae1acfe
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-08-08 20:53:13 +0300

    update

--- FirstApp/my-app/src/App.js
+++ FirstApp/my-app/src/App.js
@@ -2,17 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Login from './components/Login';
+import User from './components/User';
 
-const isLoggedIn = true;
-const fullname = "Ebubekir";
+// const isLoggedIn = true;
+// const fullname = "Ebubekir";
  
 
 function App() {
-  return <div className="test"> 
-     {
-      isLoggedIn ?  <div> Hoş Geldin {fullname} </div>  : <Login/>
-     }
-    </div>;
+  // return <div className="test"> 
+  //    {
+  //     isLoggedIn ?  <div> Hoş Geldin {fullname} </div>  : <Login/>
+  //    }
+  //   </div>;
+
+  return <div>
+    <User/>
+  </div>
 }
 
 export default App;
