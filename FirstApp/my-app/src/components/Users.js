d3c2b03afd8d4be4b32091b2df0d93c9dcd14843
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-08-16 21:03:45 +0300

    axios library add

--- FirstApp/my-app/src/components/Users.js
+++ FirstApp/my-app/src/components/Users.js
@@ -1,21 +1,37 @@
 import React,{useEffect,useState} from 'react'
+import axios from "axios"
 
 function Users() {
+
   const [users,setUsers] = useState([]);
+  const [loading,setLoading] = useState(true);
+  
+  
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //   .then((response) => response.json())
+  //   .then((data) => setUsers(data))
+  //   .finally(() => setLoading(false))
+  // },[]);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((data) => setUsers(data))
+    axios("https://jsonplaceholder.typicode.com/users")
+    .then((response) => setUsers(response.data))
+    .finally(() => setLoading(false))
   },[]);
 
     return (
     <div>
       <h2>Users</h2>
 
+      {
+        loading && <div>YÜKLENİYOR</div>
+      }
       <ul>
         {users.map((user) => (
             <li key= {user.id}>{user.name}</li>  
         ))}
+        
        
       </ul>  
     </div>
