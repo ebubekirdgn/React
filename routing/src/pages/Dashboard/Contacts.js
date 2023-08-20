1e564d94521da4256a001ec600d570de23cc950a
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-08-20 23:37:24 +0300

    add formik library

--- routing/src/pages/Dashboard/Contacts.js
+++ routing/src/pages/Dashboard/Contacts.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { Formik,Field,Form } from 'formik';
 
 function Contacts() {
   return (
     <div>
-        <h2>Contacts</h2>
+      <h2>Contacts</h2>
+      <Formik
+        initialValues={{ firstName: "", lastName: "", email: "" }}
+        onSubmit={(values) => {
+          console.log(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
-  )
+  );
 }
 
 export default Contacts
