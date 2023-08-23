1730d508759eacd82be34dbdff78fc1f0ca4b238
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-08-23 23:20:05 +0300

    isFormatting and resetForm

--- routing/src/pages/Dashboard/Contacts.js
+++ routing/src/pages/Dashboard/Contacts.js
@@ -1,61 +1,72 @@
-import React from 'react'
-import {useFormik } from 'formik';
+import React from "react";
+import { useFormik } from "formik";
 
 function Contacts() {
-  const {handleSubmit,handleChange} = useFormik({
-    initialValues:{ firstname: "", lastName: "", email: "",message:"" },
-    onSubmit:(values) => {
+  const { handleSubmit, handleChange,values ,isSubmitting} = useFormik({
+    initialValues: { firstname: "", lastName: "", email: "", message: "" },
+    onSubmit: async (values,bag) => {
+      await new Promise((r) => {
+        setTimeout(r,1000)
+        
+      })
+      console.log(bag); // Formla alakalı yapabileceğimiz  cogu sey gelir burada
       console.log(values);
-    }
+      bag.resetForm();
+    },
   });
 
   return (
     <div>
       <h2>Contacts</h2>
-          <form onSubmit={handleSubmit} className='form'>
-            <div>
-              <label htmlFor="firstname">First Name</label>
-              <input
-                id="firstname"
-                name="firstname"
-                placeholder="firstname"
-                onChange={handleChange("firstname")}
-              />
-            </div>
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <label htmlFor="firstname">First Name</label>
+          <input
+            id="firstname"
+            name="firstname"
+            placeholder="firstname"
+            value={values.firstname}
+            onChange={handleChange("firstname")}
+          />
+        </div>
 
-            <div>
-              <label htmlFor="lastName">Last Name</label>
-              <input
-                id="lastName"
-                name="lastName"
-                placeholder="lastName"
-                onChange={handleChange("lastName")}
-              />
-            </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            placeholder="lastName"
+            value={values.lastName}
+            onChange={handleChange("lastName")}
+          />
+        </div>
 
-            <div>
-              <label htmlFor="email">Email</label>
-              <input
-                id="email"
-                name="email"
-                placeholder="jane@acme.com"
-                onChange={handleChange("email")}
-              />
-            </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            value={values.email}
+            onChange={handleChange("email")}
+          />
+        </div>
 
-            <div>
-              <label htmlFor="message">Message</label>
-              <textarea
-                id="message"
-                name="message"
-                placeholder="Message"
-                onChange={handleChange("message")}
-              />
-            </div>
-            <button type="submit">Submit</button>
-          </form>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Message"
+            value={values.message}
+            disabled={isSubmitting}
+            onChange={handleChange("message")}
+          />
+        </div>
+        <button type="submit" disabled={isSubmitting}>Submit</button>
+      </form>
     </div>
   );
 }
 
-export default Contacts
+export default Contacts;
