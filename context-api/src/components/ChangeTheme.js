a85c727b14e4539d84d7c87438492667a554d5cf
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-08-26 18:43:49 +0300

    context optimization

--- context-api/src/components/ChangeTheme.js
+++ context-api/src/components/ChangeTheme.js
@@ -1,8 +1,7 @@
-import React, { useContext } from "react";
-import ThemeContext from "../contexts/ThemeContext";
+import {useTheme} from "../contexts/ThemeContext";
 
 function ChangeTheme() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
+  const { theme, toggleTheme } = useTheme();
   return (
     <div>
       <div>Aktif Tema : {theme}</div>
