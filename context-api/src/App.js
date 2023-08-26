3e143db2306a450e7975a31ace2934f25b737baa
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-08-26 15:34:21 +0300

    multiple cobtext add

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,14 +1,21 @@
+import ChangeLang from "./components/ChangeLang";
 import ChangeTheme from "./components/ChangeTheme";
 import Footer from "./components/Footer";
+import { LangContextProvider } from "./contexts/LangContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div>
-      <ThemeContextProvider value={"light"}>
-        <ChangeTheme />
-        <Footer />
-      </ThemeContextProvider>
+      <LangContextProvider>
+        <ThemeContextProvider value={"light"}>
+          <ChangeTheme />
+          <hr></hr>
+          <ChangeLang></ChangeLang>
+          <Footer />
+        </ThemeContextProvider>
+      </LangContextProvider>
+
     </div>
   );
 }
---
+++ context-api/src/components/ChangeLang.js
@@ -0,0 +1,18 @@
+import React,{useContext} from 'react'
+import LangContext from '../contexts/LangContext';
+
+function ChangeLang() {
+const {lang,setLang} = useContext(LangContext);
+    return (
+    <div>
+        Aktif Dil : {lang}
+        <div>
+            <button onClick={() => setLang('TR')}> TR </button>
+            <button onClick={() => setLang('EN')}> EN </button>
+            <button onClick={() => setLang('DE')}> DE </button>
+        </div>
+    </div>
+  )
+}
+
+export default ChangeLang
