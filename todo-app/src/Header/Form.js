dcb685ba9cdd2d0fe0dfe57ea6324d35ab44f71c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-08-27 12:23:10 +0300

    formik and yup install

---
+++ todo-app/src/Header/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        	<form>
+           	 <input className="new-todo" placeholder="what needs to be done?" autoFocus></input>
+            </form>
+    </div>
+  )
+}
+
+export default Form
